6aa329dddc0005be128a5b0d72d266120ca034c2
RD <RD@DESKTOP-E8DPTD4>
2020-03-10 08:52:48 +0530

    digit count calculation

--- Core/src/core/practical/cases/PrimeNumberChecker.java
+++ Core/src/core/practical/cases/PrimeNumberChecker.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class PrimeNumberChecker {
 
-	static void isPrime(long input) {
+	static int isPrime(long input) {
 		int i = 0;
 		int num = 0;
+		int count =0;
 
 		for (i = 1; i <= input; i++) {
 			int counter = 0;
@@ -16,10 +17,12 @@ public class PrimeNumberChecker {
 				}
 			}
 			if (counter == 2) {
-				System.out.print(i + " ");
+				count++;
+				System.out.print(i +" ");
 			}
 
 		}
+		return count;
 	}
 
 	public static void main(String args[]) {
@@ -27,9 +30,10 @@ public class PrimeNumberChecker {
 		System.out.println("kindly enter the input :");
 		long input = scan.nextLong();
 		if (input == 0||input ==1)
-			System.out.println("enter the valid input ");
+			System.out.println("enter the valid input");
 		else {
-			isPrime(input);
+		int count =	isPrime(input);
+		System.out.println("\nthere are "+count+" prime numbers in the "+input);
 		}
 
 		scan.close();
